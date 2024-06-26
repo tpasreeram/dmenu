731837689dcd55a30645aafebcc4f51741b97798
Sreeram Vasudevan <tpasreeram@gmail.com>
2024-06-26 02:55:50 -0400

    Revert "fuzzyhighlight"
    This reverts commit 207b11d0bd7b6e75332d03157a82946d8126beef.

--- config.def.h
+++ config.def.h
@@ -12,11 +12,9 @@ static const char *fonts[] = {
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#908caa", "#191724" },
-	[SchemeSel]  = { "#e0def4", "#191724" },
-	[SchemeSelHighlight] = { "#ffc978", "#005577" },
-	[SchemeNormHighlight] = { "#ffc978", "#222222" },
-	[SchemeOut]  = { "#000000", "#00ffff" },
+	[SchemeNorm] = { "#bbbbbb", "#222222" },
+	[SchemeSel] = { "#eeeeee", "#005577" },
+	[SchemeOut] = { "#000000", "#00ffff" },
 	[SchemeHp]  = { "#bbbbbb", "#333333" }
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
